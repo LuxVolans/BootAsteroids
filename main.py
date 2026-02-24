2672e80ff338285f511391c69acbdfc7cd94f71a
LuxVolans <luxvolans@gmail.com>
2026-02-24 14:11:04 -0600

    Added player movement and rotation

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     dt = 0
 
 
@@ -21,10 +22,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+            
+        
 
         screen.fill("black")
-
-        player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
